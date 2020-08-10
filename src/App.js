7387f0e38fbab6a90ba68c547bca0ad0ad223000
rohit geddam <rohitgeddam2018@gmail.com>
2020-08-11 00:05:57 +0530

    fetching

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
-import React from 'react';
+import React ,{useEffect} from 'react';
 import Router from './Router';
-
-
-
+import axios from "axios"
 // import css file
 import './assets/css/global.css';
 import 'animate.css';
 
 function App() {
+    useEffect(() => {
+        axios.get("https://morning-dawn-50353.herokuapp.com/hello")
+        .then(res => console.log(res))
+        .catch(err => console.log(err))
+    })
     return (
         <div className="app">
 
