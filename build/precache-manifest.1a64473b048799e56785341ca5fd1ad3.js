15113a32f106595ead666915443fe5e96d993e40
rohit geddam <rohitgeddam2018@gmail.com>
2020-08-10 17:05:25 +0530

    gh pages deployed

---
+++ build/precache-manifest.1a64473b048799e56785341ca5fd1ad3.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fa861592e6c541fefa29c60cc4d3f53a",
+    "url": "/%7Bcommunity-site-client-test%7D/index.html"
+  },
+  {
+    "revision": "0da3439fe5f2f1b70011",
+    "url": "/%7Bcommunity-site-client-test%7D/static/css/2.e2dd9b0a.chunk.css"
+  },
+  {
+    "revision": "636da9dfdb429b3392a1",
+    "url": "/%7Bcommunity-site-client-test%7D/static/css/main.64182e1e.chunk.css"
+  },
+  {
+    "revision": "0da3439fe5f2f1b70011",
+    "url": "/%7Bcommunity-site-client-test%7D/static/js/2.f1dd9d9e.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "/%7Bcommunity-site-client-test%7D/static/js/2.f1dd9d9e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "636da9dfdb429b3392a1",
+    "url": "/%7Bcommunity-site-client-test%7D/static/js/main.87774b0d.chunk.js"
+  },
+  {
+    "revision": "a02c2b9cc7bde094b544",
+    "url": "/%7Bcommunity-site-client-test%7D/static/js/runtime-main.9881b8f7.js"
+  },
+  {
+    "revision": "145d0c129fd7bfb10faf97149bbdc3fc",
+    "url": "/%7Bcommunity-site-client-test%7D/static/media/dscIcon.145d0c12.png"
+  }
+]);
